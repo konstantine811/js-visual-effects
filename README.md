85fac504361513a0cb636aba0894934d8d2e6406
konstantine811 <galystyan777@gmail.com>
2023-02-27 23:41:04 +0200

    draw arrays of rectangles

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Second Lesson - Loop drawing
+
+```javascript
+drawArray() {
+    this.ctx.lineWidth = 2;
+    const width = 60;
+    const height = 60;
+    const gap = 20;
+    // draw array rectangle by for looping
+    // where first we drawing 5 columns
+    for (let i = 0; i < 5; i++) {
+        // and then we inner for looping
+        // and create 5 row
+      for (let j = 0; j < 5; j++) {
+        // translate next rect by multiple
+        // and plus to gap
+        let x = 100 + (width + gap) * i;
+        let y = 100 + (height + gap) * j;
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = "white";
+        this.ctx.rect(x, y, width, height);
+        this.ctx.stroke();
+
+        // draw inner randomly
+        if (Math.random() > 0.5) {
+          this.ctx.beginPath();
+          this.ctx.strokeStyle = "red";
+          this.ctx.rect(x + 8, y + 8, width - 16, height - 16);
+          this.ctx.stroke();
+        }
+      }
+    }
+  }
+```
--- src/utils/draw.ts
+++ src/utils/draw.ts
@@ -7,17 +7,31 @@ export class Draw {
   onDraw() {
     this.ctx.canvas.width = window.innerWidth;
     this.ctx.canvas.height = window.innerHeight;
-    this.ctx.fillStyle = "blue";
-    this.ctx.fillRect(100, 100, 400, 400);
+    // draw arrays rect
+    this.drawArray();
+  }
 
-    this.ctx.beginPath();
-    this.ctx.lineWidth = 4;
-    this.ctx.rect(100, 100, 400, 400);
-    this.ctx.stroke();
+  drawArray() {
+    this.ctx.lineWidth = 2;
+    const width = 60;
+    const height = 60;
+    const gap = 20;
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        let x = 100 + (width + gap) * i;
+        let y = 100 + (height + gap) * j;
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = "white";
+        this.ctx.rect(x, y, width, height);
+        this.ctx.stroke();
 
-    this.ctx.beginPath();
-    this.ctx.arc(300, 300, 100, 0, Math.PI * 2);
-    this.ctx.strokeStyle = "white";
-    this.ctx.stroke();
+        if (Math.random() > 0.5) {
+          this.ctx.beginPath();
+          this.ctx.strokeStyle = "red";
+          this.ctx.rect(x + 8, y + 8, width - 16, height - 16);
+          this.ctx.stroke();
+        }
+      }
+    }
   }
 }
